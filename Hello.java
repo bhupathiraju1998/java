5facbe112b364c35f416690376883c619cae9b9f
venu <bhupathiraju1998@gmail.com>
2024-10-06 17:52:09 +0530

    intial undestandings

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+//if we run javac Hello.java we get Hello.class file that
+// gets automatically created that contains the compiled code (byte code) so now we can run the code using 
+//java Hello((JVM+libararies) = inside JRE  will runned and JVM is actually responsible to run it) then the code in Hello.java will be executed
+
+
+//now javac is only for devlopment purpose if we want t o run the same code on frnd machine he nneeds to have jRE and JVM not JDK
